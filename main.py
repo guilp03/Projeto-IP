c31bc3f7eaf9903995fa240afeb95686a99cc9a4
VmtpCin <108680288+VmtpCin@users.noreply.github.com>
2022-10-05 12:33:17 -0300

    Primeira versão do Main

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((800, 400))
+pygame.display.set_caption('Zumbi')
+clock = pygame.time.Clock()
+
+test_surface = pygame.Surface((10, 10))
+test_surface.fill('White')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    screen.blit(test_surface, (400, 200))
+
+    pygame.display.update()
+    clock.tick(60)
