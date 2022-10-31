1b3a90c02609f5f96720227d3eb14f37698c22a9
guilp03 <108680176+guilp03@users.noreply.github.com>
2022-10-31 14:13:34 -0300

    tirei a lista pro negocio rodar

--- coletaveis.py
+++ coletaveis.py
@@ -3,15 +3,11 @@ from mapa import *
 
 class Coletaveis(pygame.sprite.Sprite):
     """"'Classe Coletaveis'"""
-    def __init__(self, pos, group, obstacle_sprites, lista_coletaveis):
+    def __init__(self, pos, group, obstacle_sprites):
         # Inserindo objeto no grupo camera
-        super().__init__(group, obstacle_sprites, lista_coletaveis)
+        super().__init__(group, obstacle_sprites)
         # Determinando sprite
         self.image = pygame.image.load('medkit.png').convert_alpha()
         # Determinando posição inicial
         self.rect = self.image.get_rect(topleft=pos)
         self.hitbox = self.rect.inflate(0,0)
-    
-    def colisao(self, player):
-        if player.hitbox.colliderect(self.hitbox):
-            
