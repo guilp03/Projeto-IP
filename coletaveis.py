dc7b388b3fdf518a8f33fe16e86d01c231dcb4b0
guilp03 <108680176+guilp03@users.noreply.github.com>
2022-10-31 19:56:08 -0300

    a

--- coletaveis.py
+++ coletaveis.py
@@ -3,9 +3,9 @@ from mapa import *
 
 class Coletaveis(pygame.sprite.Sprite):
     """"'Classe Coletaveis'"""
-    def __init__(self, pos, groups):
+    def __init__(self, pos, group, coletaveis):
         # Inserindo objeto no grupo camera
-        super().__init__(groups)
+        super().__init__(group, coletaveis)
         # Determinando sprite
         self.image = pygame.image.load('medkit.png').convert_alpha()
         # Determinando posição inicial
