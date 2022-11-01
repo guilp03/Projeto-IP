ae5b16827e7c45d3bbb3fd719450225093651afa
apenasjao <108680215+apenasjao@users.noreply.github.com>
2022-10-31 22:05:19 -0300

    tela do gameover
    falta o condicional de quando morrer pra completar

--- main.py
+++ main.py
@@ -13,17 +13,26 @@ class Game:
 
 		self.level = Level()
 	
+	def exibe_mensagem (self, msg, tamanho, cor):
+		fonte = pygame.font.SysFont('comicsansms', tamanho, cor)
+		mensagem = f'{msg}'
+		texto_formatado = fonte.render(mensagem, True, cor)
+		return texto_formatado
+
 	def run(self):
 		while True:
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					pygame.quit()
-					sys.exit()
-                
+					sys.exit()    
+			
 			self.screen.fill('#3f301d')
+			game_over = self.exibe_mensagem('GAME OVER',40,'#ff0000')
+			self.screen.blit(game_over, (530,50))
 			self.level.run()
 			pygame.display.update()
 			self.clock.tick(FPS)
+			
 
 if __name__ == '__main__':
 	game = Game()
