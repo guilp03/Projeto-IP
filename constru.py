c878837480963f5661e82bfa7c27332363007152
guilp03 <108680176+guilp03@users.noreply.github.com>
2022-11-06 21:14:27 -0300

    Update constru.py

--- constru.py
+++ constru.py
@@ -6,7 +6,7 @@ class Celeiro(pygame.sprite.Sprite):
     
     def __init__(self,pos,groups):
         super().__init__(groups)
-        self.image = pygame.image.load('../Projeto-IP/construcoes/celeiro..png').convert_alpha()
+        self.image = pygame.image.load('construcoes/celeiro..png').convert_alpha()
         self.image = pygame.transform.scale(self.image, (80*1.5, 81*1.5))
 
         self.rect = self.image.get_rect(topleft=pos)
